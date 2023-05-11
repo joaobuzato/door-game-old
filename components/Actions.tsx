a1329d996b86103d726a75dc73d36c15fb0341a8
Buzato <joaobuzato@gmail.com>
2023-05-10 21:12:09 -0300

    adicionando expanded texts

--- components/Actions.tsx
+++ components/Actions.tsx
@@ -3,7 +3,7 @@ import InventoryButton from "./updateInventory";
 import styles from "../styles/Actions.module.css"
 
 export default function Actions (props : {actions : Action[]}) {
-    if(props.actions.length > 0) {
+    if(props.actions && props.actions.length > 0) {
         return (<>
             <h3>Qual ação tomar?</h3>
             <div className={styles.door_container}>
--- components/ExpandedTextWrapper.tsx
+++ components/ExpandedTextWrapper.tsx
@@ -3,7 +3,7 @@ import styles from "../styles/ExpandedText.module.css"
 import ExpandedText from "./ExpandedText";
 
 export default function ExpandedTextWrapper (props : {expandedTexts : ExpandedTextType[]}) {
-    if(props.expandedTexts.length > 0) {
+    if(props.expandedTexts && props.expandedTexts.length > 0) {
         return (<>
             <div className={styles.wrapper}>
                 {props.expandedTexts.map((expandedText:ExpandedTextType) => (
