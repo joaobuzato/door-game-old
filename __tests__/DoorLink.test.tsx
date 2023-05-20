db717bb0bb8f21568940499b7744411a53e3b2ad
Buzato <joaobuzato@gmail.com>
2023-05-20 13:23:58 -0300

    adding tests to doorLink

--- __tests__/DoorLink.test.tsx
+++ __tests__/DoorLink.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { DoorLink } from "../components/DoorLink";
 
-describe("Doors container", () => {
+describe("DoorLink", () => {
   const door = {
     id: 1,
     path: "1000",
